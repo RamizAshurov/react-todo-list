81e74d975ee3bcb2c7bc14f19ffd301d008ac6ae
Ramiz Ashurov <dima-sedov-1996@mail.ru>
2023-02-28 21:07:06 +0300

    build app

--- src/components/search-panel/index.js
+++
@@ -1,9 +0,0 @@
-import "./index.css"
-
-function SearchPanel(props) {
-    const { inputText, handleInputChange } = props;
-
-    return <input className="search-panel__input" type="text" placeholder="Type to search..." value={inputText} onChange={(e) => handleInputChange(e.target.value)} />
-}
-
-export default SearchPanel
